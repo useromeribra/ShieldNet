6287226788a14eeb27c0de8c5a5d0dbc77934b47
useromeribra <useromeribra@gmail.com>
2025-07-17 16:47:04 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCjf9K9v8kMBa9SMZkkmjFOPL8ckFzT_zo",
+    authDomain: "shieldnet-web.firebaseapp.com",
+    projectId: "shieldnet-web",
+    storageBucket: "shieldnet-web.firebasestorage.app",
+    messagingSenderId: "302819055337",
+    appId: "1:302819055337:web:a24861946e176b40b7c278",
+    measurementId: "G-3H9FXJF2CJ"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  window.firebaseAuth = auth;
+</script>
